e7afbc8310d6705f5fdd2fcd18469ef23d30b024
Daniel <122861875+d2nielwow@users.noreply.github.com>
2023-02-13 01:19:49 +0500

    Update words.php

--- words.php
+++ words.php
@@ -1,1 +1,7 @@
-$words = ["red" => "красный", "blue" => "синий", "green" => "зеленый"];
+for ($i=0;$i<count($arr); $i++) {
+    // code
+}
+// в вашем случае
+for ($i=count($arr)-1;$i>=0; $i--) {
+    echo $arr[$i];
+}
